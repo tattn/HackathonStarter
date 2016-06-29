5e713d6611da2da4da71a2043595365c58219e4e
tattn <tanakasan2525@gmail.com>
2016-06-30 01:25:07 +0900

    Add a storyboard helper

---
+++ HackathonStarter/Extension/StoryboardHelper.swift
@@ -0,0 +1,25 @@
+//
+//  StoryboardHelper.swift
+//  HackathonStarter
+//
+//  Created by 田中　達也 on 2016/06/30.
+//  Copyright © 2016年 tattn. All rights reserved.
+//
+
+import UIKit
+
+protocol StoryBoardHelper {}
+
+extension StoryBoardHelper where Self: UIViewController {
+    static func instantiate() -> Self {
+        let storyboard = UIStoryboard(name: self.className, bundle: nil)
+        return storyboard.instantiateViewControllerWithIdentifier(self.className) as! Self
+    }
+
+    static func instantiate(storyboard: String) -> Self {
+        let storyboard = UIStoryboard(name: storyboard, bundle: nil)
+        return storyboard.instantiateViewControllerWithIdentifier(self.className) as! Self
+    }
+}
+
+extension UIViewController: StoryBoardHelper {}
