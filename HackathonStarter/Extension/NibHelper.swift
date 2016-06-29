ec2b10fe96d9fc6bacde4832b70e55fe71323898
tattn <tanakasan2525@gmail.com>
2016-06-30 01:26:26 +0900

    Add a nib helper

---
+++ HackathonStarter/Extension/NibHelper.swift
@@ -0,0 +1,20 @@
+//
+//  NibHelper.swift
+//  HackathonStarter
+//
+//  Created by 田中　達也 on 2016/06/30.
+//  Copyright © 2016年 tattn. All rights reserved.
+//
+
+import UIKit
+
+protocol NibHelper {}
+
+extension NibHelper where Self: UIView {
+    static func instantiate() -> Self {
+        let nib = UINib(nibName: self.className, bundle: nil)
+        return nib.instantiateWithOwner(nil, options: nil)[0] as! Self
+    }
+}
+
+extension UIView: NibHelper {}
