55fe6ce3b8d6ff5dbf1691361856fe5bf8396934
tattn <tanakasan2525@gmail.com>
2016-07-01 01:55:41 +0900

    Add an app struct

---
+++ HackathonStarter/Application/App.swift
@@ -0,0 +1,27 @@
+//
+//  App.swift
+//  HackathonStarter
+//
+//  Created by 田中　達也 on 2016/07/01.
+//  Copyright © 2016年 tattn. All rights reserved.
+//
+
+import UIKit
+
+struct App {
+    static let screenWidth = UIScreen.mainScreen().bounds.width
+    static let screenHeight = UIScreen.mainScreen().bounds.height
+    static let navigationBarHeight: CGFloat = 44
+    static let statusBarHeight = UIApplication.sharedApplication().statusBarFrame.height
+    static let statusBarAndNavigationBarHeight = App.statusBarHeight + App.navigationBarHeight
+
+    struct Color {
+        static let main = UIColor.redColor()
+    }
+
+    struct Font {
+        static func main(size: CGFloat) -> UIFont { return App.Font.hiraKakuW6(size: size) }
+        static func hiraKakuW6(size size: CGFloat) -> UIFont { return UIFont(name: "HiraKakuProN-W6", size: size) ?? UIFont.systemFontOfSize(size) }
+        static func hiraKakuW3(size size: CGFloat) -> UIFont { return UIFont(name: "HiraKakuProN-W3", size: size) ?? UIFont.systemFontOfSize(size) }
+    }
+}
