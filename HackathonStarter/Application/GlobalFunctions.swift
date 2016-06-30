c6492690fc645178351dfb645a5b9062c67e36b5
tattn <tanakasan2525@gmail.com>
2016-07-01 01:50:33 +0900

    Add a global function

---
+++ HackathonStarter/Application/GlobalFunctions.swift
@@ -0,0 +1,13 @@
+//
+//  GlobalFunctions.swift
+//  HackathonStarter
+//
+//  Created by 田中　達也 on 2016/07/01.
+//  Copyright © 2016年 tattn. All rights reserved.
+//
+
+import UIKit
+
+func print(message: String, filename: String = #file, line: Int = #line, function: String = #function) {
+    Swift.print("\((filename as NSString).lastPathComponent):\(line) \(function):\r\(message)")
+}
