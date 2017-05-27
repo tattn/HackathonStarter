0a14d25fa895638d05a783865f00107f4270e538
tattn (Tatsuya Tanaka) <tanakasan2525@gmail.com>
2017-05-28 05:18:31 +0900

    Add AppUI framework

---
+++ AppUI/AppUI.h
@@ -0,0 +1,19 @@
+//
+//  CoreUI.h
+//  CoreUI
+//
+//  Created by Tatsuya Tanaka on 20170528.
+//  Copyright © 2017年 tattn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreUI.
+FOUNDATION_EXPORT double AppUIVersionNumber;
+
+//! Project version string for CoreUI.
+FOUNDATION_EXPORT const unsigned char AppUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreUI/PublicHeader.h>
+
+
--- AppUtility/AppUtility.h
+++ AppUtility/AppUtility.h
@@ -9,10 +9,10 @@
 #import <UIKit/UIKit.h>
 
 //! Project version number for CoreUtility.
-FOUNDATION_EXPORT double CoreUtilityVersionNumber;
+FOUNDATION_EXPORT double AppUtilityVersionNumber;
 
 //! Project version string for CoreUtility.
-FOUNDATION_EXPORT const unsigned char CoreUtilityVersionString[];
+FOUNDATION_EXPORT const unsigned char AppUtilityVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <CoreUtility/PublicHeader.h>
 
