833a0fa9c1a5077301ec5cc6ec8612c679aa15f9
tattn (Tatsuya Tanaka) <tanakasan2525@gmail.com>
2017-06-02 02:08:42 +0900

    Add retry

--- HackathonStarter/Utility/Function/CoreFunctionExtension.swift
+++ HackathonStarter/Utility/Function/CoreFunctionExtension.swift
@@ -1,12 +1,12 @@
 //
-//  GlobalFunctions.swift
+//  CoreFunctionExtension.swift
 //  HackathonStarter
 //
 //  Created by 田中　達也 on 2016/07/01.
 //  Copyright © 2016年 tattn. All rights reserved.
 //
 
-import UIKit
+import Foundation
 
 func print(_ message: String, filename: String = #file, line: Int = #line, function: String = #function) {
     #if DEBUG
